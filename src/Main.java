2ca9acb298d4fe5be83a5889e3f9318f0f23db55
mikefront <mike_front@mail.ru>
2022-09-29 13:23:27 +0500

    Задание1 выполнил

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //Задание1
+        int[] numbers = new int [3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        System.out.println();
+
+        double[] someNumbers = {1.57, 7.654, 9.986};
+        System.out.println();
+
+        int[] fruitBox = new int[15];
+        fruitBox[0] = 4;
+        fruitBox[1] = 9;
+        fruitBox[2] = 15;
+        System.out.println();
+
+        //Задание2
     }
 }

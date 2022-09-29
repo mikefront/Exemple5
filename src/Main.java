923ab36b716d0d61888b5b652892ad42caee281f
mikefront <mike_front@mail.ru>
2022-09-29 14:04:09 +0500

    Задание2 выполнил

--- src/Main.java
+++ src/Main.java
@@ -18,5 +18,34 @@ public class Main {
         System.out.println();
 
         //Задание2
+        int[] diffNum = new int [3];
+        diffNum[0] = 1;
+        diffNum[1] = 2;
+        diffNum[2] = 3;
+        for (int j = 0; j < diffNum.length; j++) {
+            System.out.print(diffNum[j]);
+            if (j != diffNum.length -1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        double[] commNum = {1.57, 7.654, 9.986};
+        for (int j = 0; j < commNum.length; j++) {
+            System.out.print(commNum[j]);
+            if (j != commNum.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        int[] anyBox = new int[3];
+        anyBox[0] = 4;
+        anyBox[1] = 9;
+        anyBox[2] = 15;
+        for (int k = 0; k < anyBox.length; k++) {
+            System.out.print(anyBox[k]);
+            if (k != anyBox.length - 1) {
+                System.out.print(", ");
+            }
+        }
     }
 }
